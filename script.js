7bbfb833ffc77060cae6c20e19b658313fe3bb48
solkatt <viktoralexanderodman@gmail.com>
2019-11-21 20:49:41 +0100

    Updated Layout

--- script.js
+++ script.js
@@ -1,55 +1,48 @@
 window.addEventListener("load", mainFunction);
 
-let i = 0;
-let images = [];
-let time = 3000;
-
-images[0] = '/img/boreal_1.jpg';
-images[1] = '/img/boreal_1.jpg';
-images[2] = '/img/boreal_1.jpg';
-images[3] = '/img/boreal_1.jpg';
-
-// SLIDER FUNCTION
-
-// function changeImg(){
-//     document.slide.src = images[i];
-
-
-//     if (i < images.length - 1) {
-//         i++
-//     } else {
-//         i = 0;
-//     }
-
-//     setTimeout('changeImg()', time);
-// }
-
-
-
-
-
-
 function mainFunction() {
 
 
 
-   const scrollIcon = document.querySelector('.icon');
+  const scrollIcon = document.querySelector('.icon');
 
-   scrollIcon.addEventListener('click', scrollFunction);
-   
-    
-   function scrollFunction()
- {
-    document.querySelector('main').scrollIntoView({ 
-        behavior: 'smooth' 
-      }, 1000);
-
- }   
-   ;
+  scrollIcon.addEventListener('click', scrollFunction);
 
 
+  function scrollFunction() {
+    document.querySelector('main').scrollIntoView({
+      behavior: 'smooth'
+    }, 1000);
+  };
 
-     
+  function scrollAppear() {
+    let intro = document.querySelector('.intro');
+    let introPosition = intro.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight / 2;
+
+    if (introPosition < screenPosition) {
+      intro.classList.add('intro-appear')
+    }
+
+  }
+
+  window.addEventListener('scroll', scrollAppear);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
