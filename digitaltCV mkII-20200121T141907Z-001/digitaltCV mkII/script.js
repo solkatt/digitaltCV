60c871420a3dada5c12f6e5b595fe838e34f50d5
solkatt <viktoralexanderodman@gmail.com>
2020-01-21 19:56:10 +0100

    mk2 Init

---
+++ digitaltCV mkII-20200121T141907Z-001/digitaltCV mkII/script.js
@@ -0,0 +1,54 @@
+// let animateButton;
+// let rightButton;
+// let leftButton;
+
+// let div1;
+// let div2;
+// let div3;
+
+
+
+
+// let toggleState = 'OFF';
+
+// div1 = document.getElementById('div1');
+// div2 = document.getElementById('div2');
+
+// //Animate Button
+// animateButton = document.getElementById('animateButton');
+
+// animateButton.addEventListener('click', animation);
+// //rightButton
+// rightButton = document.getElementById('rightButton');
+
+// rightButton.addEventListener('click', animate);
+
+// //leftButton
+// leftButton = document.getElementById('leftButton');
+// leftButton.addEventListener('click', animate('left'))
+
+
+// function animation() {
+//     if (toggleState == 'OFF') {
+
+//         div1.style.transform = "translate(0, -100%)";
+//         div2.style.transform = "translate(0, -100%)";
+
+//         toggleState = 'ON';
+//     } else if (toggleState == 'ON') {
+//         div1.style.transform = "translate(0, 0)";
+//         div2.style.transform = "translate(0, 0)";
+//         toggleState = 'OFF';
+//     }
+// }
+
+// function animate() {
+
+    
+//         div2.style.transform = "translate(-100%, 0)";
+//         console.log('RIGHT WORKS')
+    
+
+// }
+
+
